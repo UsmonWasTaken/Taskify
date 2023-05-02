4415133aea6ab4d986c4fbc554dd5ca2d3dad747
Usmon Abdurakhmanov <usdaves@gmail.com>
2023-05-02 17:41:43 +0500

    `SuccessAuthRepository` and `FailureAuthRepository` dummy classes are created for #30

---
+++ auth/data/src/main/java/app/taskify/auth/data/repository/SuccessAuthRepository.kt
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ */
+
+package app.taskify.auth.data.repository
+
+import app.taskify.auth.domain.repository.AuthRepository
+import javax.inject.Inject
+
+@Suppress("unused")
+class SuccessAuthRepository @Inject constructor() : AuthRepository {
+
+  override suspend fun signIn(email: String, password: String): Result<String> {
+    return Result.success("userId")
+  }
+
+  override suspend fun signUp(email: String, password: String): Result<String> {
+    return Result.success("userId")
+  }
+}
